245361bce3b466b3dc2abbed560581213fb628c4
Teresa <tditada@gmail.com>
2015-03-27 21:24:50 -0300

    removing bin from git and package tests

---
+++ src/tests/resolveGame.java
@@ -0,0 +1,10 @@
+package tests;
+
+
+public class resolveGame {
+
+	public static void main(String[] args) throws InterruptedException {
+//		GPSEngine gps = new GPSEngineImpl();
+//		gps.engine(new GameProblem(), SearchStrategy.Greedy, Heuristic.ROWS);
+	}
+}
